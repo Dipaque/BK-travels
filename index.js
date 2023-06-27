794645f8a6075a4c8efdab9bdc6cd3c27bc2d744
Deepak R <111674608+Dipaque@users.noreply.github.com>
2023-06-27 15:41:57 +0530

    likes added

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+  let a=document.getElementById('explorenow').value;
+// 
+const d=new Date(a);
+console.log(d.getMonth()+1);
+
+// 
+
+function like(){
+  
+document.getElementById("heart").src='unlike_prev_ui.png';
+
+}
+
+function unlike(){
+  document.getElementById('heart').src='like_prev_ui.png';
+  }
+  function like1(){
+  let count=0
+    document.getElementById("heart1").src='unlike_prev_ui.png';
+   return  count+1;
+    }
+    
+    function unlike1(){
+      document.getElementById('heart1').src='like_prev_ui.png';
+      }
+      function like2(){
+  
+        document.getElementById("heart2").src='unlike_prev_ui.png';
+        
+        }
+        
+        function unlike2(){
+          document.getElementById('heart2').src='like_prev_ui.png';
+          }
+ 
+
+  
+  
+  
